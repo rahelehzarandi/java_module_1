9c64aaa1b78ec4358530fd15bb890c077d34ad4a
rahelehz <raheleh.zarandi@metropolia.fi>
2022-11-21 10:07:45 +0330

    exe10

--- exercise10.js
+++ exercise10.js
@@ -12,39 +12,49 @@ let numDice= prompt("Enter number of dice");
 numDice=parseInt(numDice);
 let sumEye= prompt("Enter sum of eye number");
 sumEye=parseInt(sumEye);
-var count=0
+var count=0;
 
-
-
-let table1 = new Array(numDice+1);
-for(let i=0;i<(numDice+1);i++)
-    {
-        table1[i]=new Array(sumEye+1);
-        for(let j=0;j<(sumEye+1);j++)
-        {
-            table1[i][j]=0;
+for (let i=0;i<10000;i++){
+    let roll=0
+    for (let j=0;j<numDice;j++){
+        roll=roll+Math.floor(Math.random()*6)+1;
+        if (roll==sumEye){
+            count++
         }
     }
-
-for(let j = 1; j <= 6 && j <= sumEye; j++)
-                table1[1][j] = 1;
-
-
-for(let i = 2; i <= numDice;i ++){
-    for(let j = 1; j <= sumEye; j++){
-        for(let k = 1; k < j && k <= 6; k++){
-            table1[i][j] += table1[i-1][j-k];
-            table1[i][j]=table1[i][j]
-            count=table1[i][j]
-
-
-                }
-        }
-
 }
-count=count/Math.pow(6,numDice)
-count=count.toFixed(2)
-count=count*100
+probablity=(count/10000)*100
 document.write("Probability to get sum"+ sumEye+" with" +numDice+" dice is  "+count+"  %" )
 
-
+// let table1 = new Array(numDice+1);
+// for(let i=0;i<(numDice+1);i++)
+//     {
+//         table1[i]=new Array(sumEye+1);
+//         for(let j=0;j<(sumEye+1);j++)
+//         {
+//             table1[i][j]=0;
+//         }
+//     }
+//
+// for(let j = 1; j <= 6 && j <= sumEye; j++)
+//                 table1[1][j] = 1;
+//
+//
+// for(let i = 2; i <= numDice;i ++){
+//     for(let j = 1; j <= sumEye; j++){
+//         for(let k = 1; k < j && k <= 6; k++){
+//             table1[i][j] += table1[i-1][j-k];
+//             table1[i][j]=table1[i][j]
+//             count=table1[i][j]
+//
+//
+//                 }
+//         }
+//
+// }
+// count=count/Math.pow(6,numDice)
+// count=count.toFixed(2)
+// count=count*100
+// document.write("Probability to get sum"+ sumEye+" with" +numDice+" dice is  "+count+"  %" )
+//
+//
